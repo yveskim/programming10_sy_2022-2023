5941ef0f27d6e4adf01f4030fe3ec7a34a579e03
yveskimcabanting <87063301+yveskim@users.noreply.github.com>
2022-11-23 15:55:03 +0800

    project

---
+++ Project/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="myStyle.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="title">
+        <div class="row">
+          <h2>Final project in Programming</h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <h4>Welcome to my Website</h4>
+          <h5>Please Login</h5>
+        </div>
+        <div class="col-md-6"  style="background-color: lightgray; padding: 2em; margin-top: 2em; border-radius: 2em;">
+          <div class="row">
+            <div class="col-md-12">
+              <label class="form-label" for="">username</label>
+              <input class="form-control" type="text" name="" value="">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <label class="form-label" for="">password</label>
+              <input class="form-control" type="password" name="" value="">
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-md-12">
+              <button class="btn btn-primary" type="button" name="button" style="width: 100%;">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.js" charset="utf-8"></script>
+    <script src="jquery/jquery-3.6.1.min.js" charset="utf-8"></script>
+  </body>
+</html>
